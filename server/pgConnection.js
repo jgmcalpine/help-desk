cc1a12231de9d8299084a08373e036256573df54
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-13 11:55:36 -0800

    Fixing the whole repo perms + gitignore

--- server/pgConnection.js
+++
@@ -1,5 +0,0 @@
-const Sequelize = require('sequelize');
-
-const connection = new Sequelize('postgres://sjewxpel:tynvapNbP-8ig-LVnhHQhdZSNf95o7kB@babar.elephantsql.com:5432/sjewxpel');
-
-module.exports = connection;
