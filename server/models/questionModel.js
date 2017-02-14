0f156d9c9df87e6a90dd8a85157db1a656ac5a17
hit1st <hit1st4731@sbcglobal.net>
2017-02-13 17:26:36 -0800

    built question_status table

--- server/models/questionModel.js
+++ server/models/questionModel.js
@@ -13,11 +13,37 @@ const Question = connection.define('questions', {
   file: {
     type: Sequelize.TEXT,
   },
-  answered: {
-    type: Sequelize.BOOLEAN,
-    defaultValue: false,
+  // question_status: {
+  //   type: Sequelize.BOOLEAN,
+  //   defaultValue: false,
+  // },
+  userid: {
+    type: Sequelize.INTEGER,
   },
 });
+
+const QuestionStatus = connection.define('question_status', {
+  opened: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  pending: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  solved: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+  need_more_clarification: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+// Will add a questionId attribute to QuestionStatus to hold the primary key
+// value for Question
+QuestionStatus.belongsTo(Question);
 
 Question.sync(
   // {
@@ -26,4 +52,6 @@ Question.sync(
   // }
 );
 
-module.exports = Question;
+QuestionStatus.sync();
+
+module.exports = { Question, QuestionStatus };
