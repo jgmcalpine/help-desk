e2c56b5b7eb189adaa7b5acc10fd3d5ca089ecd3
Rodrigo Leme <lemerodrigo@gmail.com>
2017-02-14 19:27:37 -0800

    Forms form question and responses + dashboard

--- client/components/dashboard.jsx
+++ client/components/dashboard.jsx
@@ -1,36 +1,70 @@
 import React from 'react';
-import ChatWindow from './chat-window.jsx';
-import ImageWindow from './image-window.jsx'
-import QuestionsWindow from './questions-window.jsx';
-import {router} from 'react-router';
+import Question from './question.jsx';
+import Navbar from './navbar.jsx';
+import NewQuestion from './newquestion.jsx';
 
 export default class Dashboard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.props.getQuestions();
-        this.props.getMessages();
-    }
-    render() {
-        return (
-            <div>
-                <QuestionsWindow
-                    questions={this.props.questions}
-                    userName={this.props.userName}
-                    postNewQuestion={this.props.postNewQuestion}
-                    setSelectedQuestionChat={this.props.setSelectedQuestionChat}
-                    newQuestionInputHandler={this.props.newQuestionInputHandler}
-                    newQuestionInput={this.props.newQuestionInput}
-                />
-                {/*<ImageWindow />*/}
-                {/*<ChatWindow
-                    userName={this.props.userName}
-                    chatMessages={this.props.chatMessages}
-                    id={this.props.selectedQuestionId}
-                    chatInputHandler={this.props.chatInputHandler}
-                    chatInput={this.props.chatInput}
-                    postMessage={this.props.postMessage}
-                />*/}
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const filtered1 = Object.values(this.props.getQuestions()).filter((question) => {
+      return question.status.id === 1;
+    });
+    const filtered2 = Object.values(this.props.getQuestions()).filter((question) => {
+      return question.status.id === 2;
+    });
+    const filtered3 = Object.values(this.props.getQuestions()).filter((question) => {
+      return question.status.id === 3;
+    });
+    const filtered4 = Object.values(this.props.getQuestions()).filter((question) => {
+      return question.status.id === 4;
+    });
+
+    let arr1 = [];
+    let arr2 = [];
+    let arr3 = [];
+    let arr4 = [];
+
+    filtered1.forEach((question, idx) => {
+      arr1.push(<Question key={question.id} status={1} data={question} />);
+    });
+    filtered2.forEach((question, idx) => {
+      arr2.push(<Question key={question.id} status={2} data={question} />);
+    });
+    filtered3.forEach((question, idx) => {
+      arr3.push(<Question key={question.id} status={3} data={question} />);
+    });
+    filtered4.forEach((question, idx) => {
+      arr4.push(<Question key={question.id} status={4} data={question} />);
+    });
+
+    return (
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <NewQuestion />              
             </div>
-        )
-    }
+          </div>
+          <div className="row">
+            <div className="col-lg-3">
+              {arr1}
+            </div>
+            <div className="col-lg-3">
+              {arr2}
+            </div>
+            <div className="col-lg-3">
+              {arr3}
+            </div>
+            <div className="col-lg-3">
+              {arr4}
+            </div>
+          </div>
+        </div>
+      </div>        
+    );
+  }
 }
--- client/components/question.jsx
+++ client/components/question.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
+import Response from './response.jsx';
 
 export default class Question extends React.Component {
   constructor(props) {
-    super(props)
+      super(props);
   }
 
-// onClick={this.props.setSelectedQuestionChat.bind(null, this.props.id)}
+  handleClick(e) {
+    e.preventDefault();
+    const questionId = e.target.dataset.id;
+  }
 
   render() {
+    let types = {
+      1: 'success',
+      2: 'danger',
+      3: 'warning',
+      4: 'primary',
+    };
+    let responses = [];
+    this.props.data.responses.forEach((response, idx) => {
+      responses.push(<Response key={'response' + response.id} data={response} />);
+    });
+
     return (
-      <div className="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-        <div className="panel panel-default">
+      <div className="panel-group" id={'accordion' + this.props.data.id} role="tablist" aria-multiselectable="true">
+        <div className={'panel panel-' + types[this.props.status]}>
+
           <div className="panel-heading" role="tab" id="headingOne">
             <h4 className="panel-title">
-              <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                {this.props.question} {this.props.created_at} {this.props.userName}
+              <a role="button" data-id={this.props.data.id} data-toggle="collapse" data-parent={'#accordion' + this.props.data.id} href={'#collapseOne' + this.props.data.id} aria-expanded="true" aria-controls="collapseOne">
+                {this.props.data.question}
+                {/*{this.props.data.created_at}
+                {this.props.data.userId}
+                {this.props.data.id}*/}
               </a>
             </h4>
           </div>
-          <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+          <div id={'collapseOne' + this.props.data.id} className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
             <div className="panel-body">
-              ANSWERS
+              <ul className="list-group">
+                {responses}
+              </ul>
+              <form>
+                <div className="input-group">
+                  <input type="text" className="form-control" />
+                  <span className="input-group-btn">
+                    <button className="btn btn-default" type="button">Answer</button>
+                  </span>
+                </div>
+              </form>
             </div>
           </div>
         </div>
@@ -28,6 +57,3 @@ export default class Question extends React.Component {
     );
   }
 }
-
-
-
